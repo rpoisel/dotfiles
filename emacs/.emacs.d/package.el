88f7eb2c0441aefb9e5055845d6da3298e6f1953
Rainer Poisel <rainer.poisel@gmail.com>
2021-12-03 23:09:08 +0100

    feat: LSP C/C++ base configuration added

--- emacs/.emacs.d/package.el
+++ emacs/.emacs.d/package.el
@@ -311,6 +311,27 @@
   :ensure
   :commands lsp-treemacs-errors-list)
 
+;; C/C++
+(use-package yasnippet
+  :ensure)
+(use-package hydra
+  :ensure)
+(use-package dap-mode
+  :ensure)
+(add-hook 'c-mode-hook 'lsp)
+(add-hook 'c++-mode-hook 'lsp)
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      treemacs-space-between-root-nodes nil
+      company-idle-delay 0.0
+      company-minimum-prefix-length 1
+      lsp-idle-delay 0.1)  ;; clangd is fast
+
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (require 'dap-cpptools)
+  (yas-global-mode))
+
 ;; Python
 (use-package lsp-python-ms
   :ensure
