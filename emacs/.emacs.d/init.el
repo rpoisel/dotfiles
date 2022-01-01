863e2b71f6b245d05b5a32616358c8a16f9f05b8
Rainer Poisel <rainer.poisel@gmail.com>
2022-01-01 23:26:35 +0100

    chore(Emacs): fix flycheck warnings

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,8 @@
+;;; package --- Emacs configuration
+;;; Commentary:
+;;; Rainer Poisel Emacs configuration
+;;; Code:
+
 (set-frame-font "DejaVu Sans Mono-10" nil t)
 
 (setq inhibit-startup-screen t)
@@ -11,9 +16,10 @@
 (setq make-backup-files nil)
 
 ;; Configure packages
-(setq package-file (expand-file-name "package.el" user-emacs-directory))
-(when (file-exists-p package-file)
-  (load package-file))
+(let ((package-file (expand-file-name "package.el" user-emacs-directory)))
+  (when (file-exists-p package-file)
+    (load package-file)))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -22,10 +28,6 @@
  '(evil-undo-system 'undo-tree)
  '(ispell-dictionary nil)
  '(package-selected-packages
-   '(ox-reveal org-tree-slide clang-format crux yasnippet marginalia capf-autosuggest multi-vterm visual-fill-column consult dired-subtree dired-collapse lsp-pyright git-timemachine php-mode lsp-ui projectile treemacs-evil evil-visualstar evil-surround undo-tree rgi tree-sitter zenburn-theme vertico use-package orderless evil)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(vertico-current ((t (:background "#3a3f5a")))))
+   '(rust-mode ox-hugo ox-reveal org-tree-slide clang-format crux yasnippet marginalia capf-autosuggest multi-vterm visual-fill-column consult dired-subtree dired-collapse lsp-pyright git-timemachine php-mode lsp-ui projectile treemacs-evil evil-visualstar evil-surround undo-tree rgi tree-sitter zenburn-theme vertico use-package orderless evil)))
+
+;;; init.el ends here
--- emacs/.emacs.d/package.el
+++ emacs/.emacs.d/package.el
@@ -1,18 +1,16 @@
 (require 'package)
 
-(setq use-package-hook-name-suffix nil)
-
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
-(package-initialize)
-
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(eval-when-compile
-  (require 'use-package))
+(let ((use-package-hook-name-suffix nil)
+      (package-enable-at-startup nil))
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+  (package-initialize)
+  (eval-when-compile
+    (require 'use-package)))
 
 (use-package dracula-theme
   :ensure
