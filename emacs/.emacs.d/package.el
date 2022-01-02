0a500c9f1bd64a3271d878c328c6e72aecfd9e2b
Rainer Poisel <rainer.poisel@gmail.com>
2022-01-02 16:30:25 +0100

    chore(Emacs): Remove visual fill column mode from org-mode

--- emacs/.emacs.d/package.el
+++ emacs/.emacs.d/package.el
@@ -887,15 +887,6 @@
   :ensure
   :after ox)
 
-(defun efs/org-mode-visual-fill ()
-  (setq visual-fill-column-width 100
-        visual-fill-column-center-text t)
-  (visual-fill-column-mode 1))
-
-(use-package visual-fill-column
-  :ensure
-  :hook (org-mode . efs/org-mode-visual-fill))
-
 (use-package capf-autosuggest
   :ensure)
 
