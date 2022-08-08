3027842f9580c7d2067bd9f4c18ea358e4ada947
Rainer Poisel <rainer.poisel@gmail.com>
2022-08-08 22:04:15 +0200

    feat(Emacs): install pdf-tools and org-pdftools (incl. dependencies)

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -42,7 +42,7 @@
  '(org-directory "~/Sync/org")
  '(org-trello-current-prefix-keybinding "C-c o")
  '(package-selected-packages
-   '(org-re-reveal shell-pop consult-flycheck dash rustic ox-hugo ox-reveal org-tree-slide clang-format crux yasnippet marginalia capf-autosuggest multi-vterm visual-fill-column consult dired-subtree dired-collapse lsp-pyright git-timemachine php-mode lsp-ui projectile treemacs-evil evil-visualstar evil-surround undo-tree rgi tree-sitter zenburn-theme vertico use-package orderless evil)))
+   '(org-noter-pdftools org-pdftools org-re-reveal shell-pop consult-flycheck dash rustic ox-hugo ox-reveal org-tree-slide clang-format crux yasnippet marginalia capf-autosuggest multi-vterm visual-fill-column consult dired-subtree dired-collapse lsp-pyright git-timemachine php-mode lsp-ui projectile treemacs-evil evil-visualstar evil-surround undo-tree rgi tree-sitter zenburn-theme vertico use-package orderless evil)))
 
 ;;; init.el ends here
 (custom-set-faces
--- emacs/.emacs.d/package.el
+++ emacs/.emacs.d/package.el
@@ -1120,6 +1120,54 @@
   :ensure)
 (add-hook 'org-mode-hook #'bug-reference-mode)
 
+(use-package pdf-tools
+  :ensure)
+(pdf-tools-install)
+
+(use-package org-noter
+  :ensure
+  :config
+  (require 'org-noter-pdftools))
+
+(use-package org-pdftools
+  :ensure
+  :hook (org-mode-hook . org-pdftools-setup-link))
+
+(use-package org-noter-pdftools
+  :ensure
+  :after org-noter
+  :config
+  ;; Add a function to ensure precise note is inserted
+  (defun org-noter-pdftools-insert-precise-note (&optional toggle-no-questions)
+    (interactive "P")
+    (org-noter--with-valid-session
+     (let ((org-noter-insert-note-no-questions (if toggle-no-questions
+                                                   (not org-noter-insert-note-no-questions)
+                                                 org-noter-insert-note-no-questions))
+           (org-pdftools-use-isearch-link t)
+           (org-pdftools-use-freepointer-annot t))
+       (org-noter-insert-note (org-noter--get-precise-info)))))
+
+  ;; fix https://github.com/weirdNox/org-noter/pull/93/commits/f8349ae7575e599f375de1be6be2d0d5de4e6cbf
+  (defun org-noter-set-start-location (&optional arg)
+    "When opening a session with this document, go to the current location.
+With a prefix ARG, remove start location."
+    (interactive "P")
+    (org-noter--with-valid-session
+     (let ((inhibit-read-only t)
+           (ast (org-noter--parse-root))
+           (location (org-noter--doc-approx-location (when (called-interactively-p 'any) 'interactive))))
+       (with-current-buffer (org-noter--session-notes-buffer session)
+         (org-with-wide-buffer
+          (goto-char (org-element-property :begin ast))
+          (if arg
+              (org-entry-delete nil org-noter-property-note-location)
+            (org-entry-put nil org-noter-property-note-location
+                           (org-noter--pretty-print-location location))))))))
+  (with-eval-after-load 'pdf-annot
+    (add-hook 'pdf-annot-activate-handler-functions #'org-noter-pdftools-jump-to-note)))
+
+
 ;; Functions
 
 ;; https://emacsredux.com/blog/2013/03/27/indent-region-or-buffer/
