29e5b895ac8351def2343498bf2f5760ac1ee7cf
Rainer Poisel <rainer.poisel@gmail.com>
2022-12-26 17:39:04 +0100

    feat(awesomewm): enable autostart of frequently used applications/applets

--- awesome/.config/awesome/autorun.sh
+++ awesome/.config/awesome/autorun.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
+function run() {
+  if ! pgrep -f "$1"; then
+    "$@"&
+  fi
+}
+
 setxkbmap -layout "us,de" -option "grp:caps_toggle"
+
+run nm-applet
+run padevchooser
+run blueman-applet
