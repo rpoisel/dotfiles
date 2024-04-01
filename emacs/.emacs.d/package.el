fc85392baa0529c8aad7e880448c8c38f8435f59
Rainer Poisel <rainer@honeytreelabs.com>
2024-04-01 16:05:50 +0200

    fix(Emacs): prevent automatic scrolling in eat-mode

--- emacs/.emacs.d/package.el
+++ emacs/.emacs.d/package.el
@@ -896,6 +896,7 @@
   (setopt eat-shell-prompt-annotation-delay 0)
   (setopt eat-very-visible-cursor-type '(t nil nil))
   (setopt eat-default-cursor-type '(t nil nil)))
+(add-hook `eat-mode-hook (lambda () (setq-local scroll-conservatively 10000)))
 
 (defun vmacs-auto-exit (buf event)
   ;; buf unused
