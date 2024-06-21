a1e68df0af37825f87dcb9a1f56d56d350336cb1
Rainer Poisel <rainer@honeytreelabs.com>
2024-06-21 14:03:49 +0200

    chore(Emacs): format org duration format to hours and minutes (not: days)

--- emacs/.emacs.d/package.el
+++ emacs/.emacs.d/package.el
@@ -1070,6 +1070,7 @@
   (setq org-outline-path-complete-in-steps nil)         ; Refile in a single go
   (setq org-refile-use-outline-path t)                  ; Show full paths for refiling
   (setq org-clock-idle-time 15)
+  (setq org-duration-format 'h:mm)
   ;; open directory links in emacs (see: https://emacs.stackexchange.com/a/10696/36387)
   (add-to-list 'org-file-apps '(directory . emacs))
   (define-key org-mode-map (kbd "C-c C-x C-r") 'org-clock-report))
